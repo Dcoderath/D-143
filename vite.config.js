0b20090e95feb2058bc8ed1919d05150943559dc
dcoderath <trivedi2693@gmail.com>
2025-08-20 15:55:58 +0530

    update for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/notyra/", // repo name here
+  base: "/D-143/", // 👈 repo name here
 });
